2d643e8b0e212e18fc42fa46d6282f76657cbdc6
cerensolpan <cerensolpan@gmail.com>
2022-03-10 00:55:43 +0300

    Testing have header

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+describe("Tests", () => {
+  let header;
+
+  beforeEach(() => {
+    render(<App />);
+    header = screen.getByText("Emoji Search");
+  });
+
+  it("should have header", () => {
+    expect(header).toBeInTheDocument();
+  });
 });
