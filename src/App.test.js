2fcc5de30dd4a5acdc59b4d5986d3ed0d36e0a77
cerensolpan <cerensolpan@gmail.com>
2022-03-11 00:50:38 +0300

    Test filter according to search

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import userEvent from "@testing-library/user-event"
 
 import App from "./App";
 
 describe("Tests", () => {
-  let header, results;
+  let header, results, input, filteredEmoji;
 
   beforeEach(() => {
     render(<App />);
@@ -17,7 +18,15 @@ describe("Tests", () => {
   });
 
   it("should be render success emoji list",()=>{
-    results= screen.getAllByTestId("item");
+    results = screen.getAllByTestId("item");
     expect(results.length).toBeGreaterThan(0);
+  })
+
+  it("should be filter according to search",()=>{
+    const emoji="grin"
+    input = screen.getByTestId("inputsearch");
+    userEvent.type(input,emoji);
+    results = screen.getAllByTestId("item");
+    expect(results.length).toEqual(3);
   })
 });
