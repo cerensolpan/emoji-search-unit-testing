6627b0769fc15ccdccbd74bc768f4d5e561304dd
cerensolpan <cerensolpan@gmail.com>
2022-03-12 01:38:28 +0300

    Test copy the emoji

--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,12 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event"
+import '@testing-library/jest-dom';
 
 import App from "./App";
 
 describe("Tests", () => {
-  let header, results, input, filteredEmoji;
+  let header, results, input, smiley;
 
   beforeEach(() => {
     render(<App />);
@@ -28,5 +29,14 @@ describe("Tests", () => {
     userEvent.type(input,emoji);
     results = screen.getAllByTestId("item");
     expect(results.length).toEqual(3);
+  })
+
+  it("should be copy the emoji",()=>{
+    document.execCommand = jest.fn();
+    smiley = screen.getByText("Smiley");
+    userEvent.click(smiley);
+    expect(document.execCommand).toBeCalledWith('copy') 
+    const copyEmoji = window.ClipboardData; 
+    expect(copyEmoji).toEqual(smiley.value);
   })
 });
